0c8458a8d6c93837c3c9cb647f3e6f4cbecdcf75
zhangwb <zhangwb@shinetechchina.com>
2020-12-08 20:11:01 +0800

    Update consumer and producer. Accept more parameters.

--- commands/consumer.sh
+++ commands/consumer.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 /opt/kafka/bin/kafka-console-consumer.sh \
---bootstrap-server $KAFKA_INTERNET_HOST:$KAFKA_INTERNET_PORT --topic $1 \
+--bootstrap-server $KAFKA_INTERNET_HOST:$KAFKA_INTERNET_PORT \
 --consumer.config /configs/client-ssl-active.properties \
---property print.key=true --property print.timestamp=true
+--property print.key=true --property print.timestamp=true \
+--topic $@
--- commands/producer.sh
+++ commands/producer.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 /opt/kafka/bin/kafka-console-producer.sh \
---bootstrap-server $KAFKA_INTERNET_HOST:$KAFKA_INTERNET_PORT --topic $1 \
---producer.config /configs/client-ssl-active.properties
+--bootstrap-server $KAFKA_INTERNET_HOST:$KAFKA_INTERNET_PORT \
+--producer.config /configs/client-ssl-active.properties \
+--topic $@
