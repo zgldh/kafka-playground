03fd253f8c0f351a061782dd96c74fbc780a2a45
zhangwb <zhangwb@shinetechchina.com>
2020-12-08 17:54:03 +0800

    consumer will show timestamp and key

--- commands/consumer.sh
+++ commands/consumer.sh
@@ -2,4 +2,5 @@
 
 /opt/kafka/bin/kafka-console-consumer.sh \
 --bootstrap-server $KAFKA_INTERNET_HOST:$KAFKA_INTERNET_PORT --topic $1 \
---consumer.config /configs/client-ssl-active.properties
+--consumer.config /configs/client-ssl-active.properties \
+--property print.key=true --property print.timestamp=true
