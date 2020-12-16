f8b1d6e71541dcaf2b546c29181067fe9ac54287
zhangwb <zhangwb@shinetechchina.com>
2020-12-16 17:30:19 +0800

    Support plaintext

--- commands/client-entrypoint.sh
+++ commands/client-entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 sed "s/\${KAFKA_SSL_KEY_PASSWORD}/$KAFKA_SSL_KEY_PASSWORD/" /configs/client-ssl.properties > /configs/client-ssl-active.properties
+sed "s/\${KAFKA_SECURITY_PROTOCOL}/$KAFKA_SECURITY_PROTOCOL/" /configs/client-ssl-active.properties > /configs/client-ssl-active.properties
+
 bash
